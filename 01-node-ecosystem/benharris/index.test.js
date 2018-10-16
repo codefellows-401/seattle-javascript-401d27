2a46769ac21bd6733edbd542cd29a6e7e4fc669b
Ben Harris <ben@harrishills.com>
2018-10-16 08:54:00 -0700

    Adding updated test scripts

--- 01-node-ecosystem/benharris/index.test.js
+++ 01-node-ecosystem/benharris/index.test.js
@@ -2,19 +2,106 @@
 'use strict';
 
 // Require modules
-const greet = require('./lib/greet');
-const arithmetic = require('./lib/arithmetic');
-const faker = require('faker');
+const greet = require('./lib/greet.js');
+const arithmetic = require('./lib/arithmetic.js');
 
-// Setup Variables
-let randomName = faker.name.findName();
-
-// Test 1
-test('outputs as string', () => {
-  expect(greet(randomName)).toBe(typeof randomName === 'string');
+// Greet Tests
+describe('given a nonstring value', () => {
+  test('returns as null', () => {
+    const actual = greet(123);
+    const expected = null;
+    expect(actual).toBe(expected);
+  });
 });
 
-// Test 2
-test('hello world', () => {
-  expect(greet(world)).toBe('Hello world');
+describe('given a specific string value', () => {
+  test('returns hello world', () => {
+    const actual = greet('world');
+    const expected = 'Hello world';
+    expect(actual).toBe(expected);
+  });
+});
+
+// Valid Arithmetic Tests
+describe('given a valid number of parameters (arity of 2)', () => {
+  test('successfully adds', () => {
+    const actual = add(1,2);
+    const expected = 3;
+    expect(actual).toBe(expected);
+  });
+
+  test('successfully substracts', () => {
+    const actual = sub(3,2);
+    const expected = 1;
+    expect(actual).toBe(expected);
+  });
+
+  test('successfully multiplies', () => {
+    const actual = multi(2,4);
+    const expected = 8;
+    expect(actual).toBe(expected);
+  });
+
+  test('successfully divides', () => {
+    const actual = div(4,2);
+    const expected = 2;
+    expect(actual).toBe(expected);
+  });
+});
+
+// Invalid Arithmetic Tests
+describe('given an invalid number of parameters', () => {
+  test('returns as null', () => {
+    const actual = add(1);
+    const expected = null;
+    expect(actual).toBe(expected);
+  });
+
+  test('returns first two parameters only', () => {
+    const actual = add(1,2,3);
+    const expected = 3;
+    expect(actual).toBe(expected);
+  });
+
+  test('returns as null', () => {
+    const actual = sub(1);
+    const expected = null;
+    expect(actual).toBe(expected);
+  });
+
+  test('returns first two parameters only', () => {
+    const actual = sub(5,1,3);
+    const expected = 4;
+    expect(actual).toBe(expected);
+  });
+
+  test('returns as null', () => {
+    const actual = multi(1);
+    const expected = null;
+    expect(actual).toBe(expected);
+  });
+
+  test('returns first two parameters only', () => {
+    const actual = multi(1,2,3);
+    const expected = 2;
+    expect(actual).toBe(expected);
+  });
+
+  test('returns as null', () => {
+    const actual = div(6);
+    const expected = null;
+    expect(actual).toBe(expected);
+  });
+
+  test('returns first two parameters only', () => {
+    const actual = div(8,2,3);
+    const expected = 4;
+    expect(actual).toBe(expected);
+  });
+
+  test('returns null when dividing by zero', () => {
+    const actual = div(3,0);
+    const expected = null;
+    expect(actual).toBe(expected);
+  });
 });
